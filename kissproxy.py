f5d74c80a15451debe97a96a858afdf42f7993f8
Jonathan Harvey-Buschel <jharveyb@mit.edu>
2020-03-26 23:18:44 -0400

    Refactor into a class

--- kissproxy.py
+++ kissproxy.py
@@ -8,21 +8,37 @@ import pyfldigi
 
 # default ports: 7322 for ARQ, 7342 for TCP/IP, 7362 for XML, 8421 for fllog
 
-hostip = '127.0.0.1'
-xmlport = 7362
-tcpport = 7342
-start_delay = 5
-stop_delay = 3
+class fl_instance:
+    host_ip = '127.0.0.1'
+    xml_port = 7362
+    tcp_port = 7342
+    start_delay = 5
+    stop_delay = 3
 
-flclient = pyfldigi.Client(hostname=hostip, port=xmlport)
-flapp = pyfldigi.ApplicationMonitor(hostname=hostip, port=xmlport)
+    # we assume no port collisions / no other instance of fldigi is running
+    # TODO: check for other fldigi instances before starting
+    def __init__(self, host=host_ip, port=xml_port, headless=False, wfall_only=False, start_delay=start_delay):
+        self.host_ip = host
+        self.xml_port = port
+        self.start_delay = start_delay
+        self.fl_client = pyfldigi.Client(hostname=self.host_ip, port=self.xml_port)
+        self.fl_app = pyfldigi.ApplicationMonitor(hostname=self.host_ip, port=self.xml_port)
+        self.fl_app.start(headless=headless, wfall_only=wfall_only)
+        sleep(self.start_delay)
 
-# ideally check if fldigi is already running with something ps -ef | grep fldigi flavored
-# but we assume it isn't
+    def port_info(self):
+        print("IP", self.host_ip, "XML-RPC port:", self.xml_port, "TCP port:", self.tcp_port)
 
-flapp.start(headless=False, wfall_only=False)
-sleep(start_delay)
-print(flclient.version)
-sleep(stop_delay)
-# both terminate & stop ask for confirmation via pop-up, so need to force kill
-flapp.kill()
+    def version(self):
+        return self.fl_client.version
+
+
+    def stop(self):
+        self.fl_client.terminate(save_options=True)
+        sleep(self.stop_delay)
+        self.fl_app.kill()
+
+fl_main = fl_instance()
+print(fl_main.version())
+fl_main.port_info()
+fl_main.stop()
