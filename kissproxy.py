171922917eba9a83200df916831ee16f4c1af6c0
Jonathan Harvey-Buschel <jharveyb@mit.edu>
2020-03-31 16:01:15 -0400

    Update tests, test binary->base64

--- kissproxy.py
+++ kissproxy.py
@@ -111,18 +111,32 @@ def base64_to_raw(base64_bytes):
     return codecs.decode(base64_bytes, 'base64')
 
 async def test_standard(fl_digi):
-    test_strings = ["TEST TEST TEST", "\n",
-        "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.", "\n",
-        "The computer can be used as a tool to liberate and protect people, rather than to control them.", "\n"]
+    test_strings = ["TEST TEST TEST\n",
+        "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.\n",
+        "The computer can be used as a tool to liberate and protect people, rather than to control them.\n"]
     for teststr in test_strings:
-        await fl_digi.send(teststr)
+        await fl_digi.radio_send(teststr)
 
 async def test_base64(fl_digi):
-    test_all_bytes = bytes(range(255))
-    kiss_sequence = 'C000DBDCDBDDC0'
-    test_decode = codecs.encode(codecs.decode(kiss_sequence, 'hex'), 'base64')
-    test_b64 = test_decode.decode()
-    await fl_digi.send(test_b64)
+    # Lightning handshake messages captured from lnproxy
+    test_handshake_0 = b'\x00\x03U\xc7\xaa\xa3\x85\xe8%\x95M\x96\xcbQ\x80C\x04\x0f\xf0\x14\xcf\x10\x11{t\x93=\x9d}\xa8a\xf5r\x02w\xca;\x11\xa1T\xaa\x81\xbf\xf2\xcbr\xd5;\xa9\xb2'
+    test_handshake_1 = b'\x00\x02L\xdf\xd9\x81\x98\xcfr\xd8\xa7d\xd2\x167\x98\xff\x9b\t\x16\x1cR\x82^\x96\t8\xfb[\x9fv\x15d\n\xc0\xf7Wi\xf2\x1f\x9f\xd6ht\xba.\xf0>\\\x1c'
+    test_handshake_2 = b'\x00\xae);;\xcd\x02\xea\x12A\xfc@\xb6L\xd6\xd2.\x8by\xfc\xddIR\xd9\x9e\x86\x96j\xbf\x8cA\xec\x8aD\xb0\xf1\xcb\xd6\xedQzq\xc3,\xb3W_\xf25\x0b\x066j\xd7\x06\xd3\xa0\xf0i=\xcd\xd8J\xb0\xffv'
+    test_handshake_3 = b'\x00-\x00\x10\x00\x02"\x00\x00\x03\x02\xaa\xa2\x01 \x06"nF\x11\x1a\x0bY\xca\xaf\x12`C\xeb[\xbf(\xc3O:^3*\x1f\xc7\xb2\xb7<\xf1\x88\x91\x0f'
+    handshakes = [test_handshake_0, test_handshake_1, test_handshake_2, test_handshake_3]
+    hs_test = True
+    for hs_message in handshakes:
+        hs_base64 = raw_to_base64(hs_message)
+        print(hs_base64)
+        hs_raw = base64_to_raw(hs_base64)
+        if (hs_raw != hs_message):
+            print("encode/decode fail!")
+            hs_test = False
+            break
+    if (hs_test == True):
+        for hs_message in handshakes:
+            hs_base64 = raw_to_base64(hs_message)
+            await fl_digi.radio_send(hs_base64)
 
 async def main():
     parser = argparse.ArgumentParser(description='Talk to fldigi.')
