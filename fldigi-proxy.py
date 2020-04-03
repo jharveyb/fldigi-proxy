d498cabad779c873c1ea5b3e51b2cf35a868b968
willcl-ark <will8clark@gmail.com>
2020-04-03 16:50:32 +0100

    Modify radio_receive
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- fldigi-proxy.py
+++ fldigi-proxy.py
@@ -1,44 +1,65 @@
-#!/usr/bin/python3.8
+#!/usr/bin/env python
 
-from time import sleep
-from collections import deque
 import argparse
-import pyfldigi
 import codecs
+from collections import deque
+from time import sleep
+
+import pyfldigi
 import trio
+
 
 # Test starting of fldigi, text encode/decode, and accepting messages to pass to fldigi
 # over a TCP/IP socket
 
 # default ports: 7322 for ARQ, 7342 for TCP/IP, 7362 for XML, 8421 for fllog
 
+
 class fl_instance:
-    host_ip = '127.0.0.1'
+    host_ip = "127.0.0.1"
     xml_port = 7362
     proxy_port = 22
     start_delay = 5
     stop_delay = 3
     poll_delay = 0.05
-    base64_prefix = b'BTC'
+    base64_prefix = b"BTC"
 
     # we assume no port collisions for KISS, ARQ, or XMLRPC ports
     # TODO: check ports before starting
-    def __init__(self, nodaemon=False, noproxy=False, host=host_ip, xmlport=xml_port,
-        proxyport=proxy_port, headless=False, wfall_only=False, start_delay=start_delay):
+    def __init__(
+        self,
+        no_daemon=False,
+        no_proxy=False,
+        host=host_ip,
+        xml_port=xml_port,
+        proxy_port=proxy_port,
+        headless=False,
+        wfall_only=False,
+        start_delay=start_delay,
+    ):
         self.host_ip = host
-        if (xmlport != None):
-            self.xml_port = xmlport
-        if (noproxy == False):
-            self.proxy_port = proxyport
+        if xml_port is not None:
+            self.xml_port = xml_port
+        if not no_proxy:
+            self.proxy_port = proxy_port
         self.start_delay = start_delay
         self.fl_client = pyfldigi.Client(hostname=self.host_ip, port=self.xml_port)
-        self.fl_app = pyfldigi.ApplicationMonitor(hostname=self.host_ip, port=self.xml_port)
-        if (nodaemon == False):
+        self.fl_app = pyfldigi.ApplicationMonitor(
+            hostname=self.host_ip, port=self.xml_port
+        )
+        if not no_daemon:
             self.fl_app.start(headless=headless, wfall_only=wfall_only)
         sleep(self.start_delay)
 
     def port_info(self):
-        print("IP", self.host_ip, "XML-RPC port:", self.xml_port, "proxy port:", self.proxy_port)
+        print(
+            "IP",
+            self.host_ip,
+            "XML-RPC port:",
+            self.xml_port,
+            "proxy port:",
+            self.proxy_port,
+        )
 
     def version(self):
         return self.fl_client.version
@@ -55,19 +76,21 @@ class fl_instance:
         self.fl_client.text.clear_tx()
         self.fl_client.main.tx()
         self.fl_client.text.add_tx(tx_msg)
+        print("Sending:", tx_msg)
         # Poll and check TX'd data; reassemble & end send once msg made it out
         byteflag = isinstance(tx_msg, bytes)
-        tx_confirm_msg = ''
+        tx_confirm_msg = ""
         tx_confirm_fragment = []
-        print("Sending:", tx_msg)
-        while (tx_msg != tx_confirm_msg):
-            await trio.sleep(self.poll_delay)
-            tx_confirm_fragment = self.fl_client.text.get_tx_data()
-            if (tx_confirm_fragment != ''):
-                if (tx_confirm_fragment.decode("utf-8") == '\n'):
-                    break
-                else:
-                    tx_confirm_msg += tx_confirm_fragment.decode("utf-8")
+        await trio.sleep(15)
+        # while tx_msg != tx_confirm_msg:
+        #     await trio.sleep(self.poll_delay)
+        #     tx_confirm_fragment = self.fl_client.text.get_tx_data()
+        #     print(f"Sent: {tx_confirm_fragment}")
+        #     if tx_confirm_fragment != "":
+        #         if tx_confirm_fragment.decode("utf-8") == "\n":
+        #             break
+        #         else:
+        #             tx_confirm_msg += tx_confirm_fragment.decode("utf-8")
         print("Sent!")
         await trio.sleep(self.poll_delay)
         self.fl_client.main.abort()
@@ -77,11 +100,11 @@ class fl_instance:
         for tx_msg in tx_msg_list:
             await self.radio_send(tx_msg)
 
-    async def radio_send_task(self, packet_deque: deque): 
+    async def radio_send_task(self, packet_deque: deque):
         print("started radio_send_task")
         radio_buffer = bytes()
-        while(True):
-            if (len(packet_deque) > 0):
+        while True:
+            if len(packet_deque) > 0:
                 radio_buffer = packet_deque.popleft()
                 await self.radio_send(radio_buffer)
             else:
@@ -93,64 +116,84 @@ class fl_instance:
         rx_prefix = bytes()
         rx_prefix_check = False
         rx_fragment = bytes()
-        while (True):
+        while True:
             await trio.sleep(self.poll_delay)
             rx_fragment = self.fl_client.text.get_rx_data()
             # empty reads are strings, not bytes
-            if (isinstance(rx_fragment, bytes) and rx_fragment != b''):
-                if (rx_prefix_check == False):
-                    rx_prefix += rx_fragment
-                    if (rx_prefix.endswith(self.base64_prefix)):
-                        rx_prefix_check = True
-                # all data before prefix excluded from message, including noise from keying up radio
-                else: 
-                    if (rx_fragment == b'\n'):
-                        break
-                    elif (rx_fragment == b'\r'):
-                        pass
-                    else:
-                        rx_msg += rx_fragment
+            # if isinstance(rx_fragment, bytes) and rx_fragment != b"":
+            #     if not rx_prefix_check:
+            #         rx_prefix += rx_fragment
+            #         if rx_prefix.endswith(self.base64_prefix):
+            #             rx_prefix_check = True
+            #     # all data before prefix excluded from message, including noise from
+            #     # keying up radio
+            #     else:
+            if rx_fragment.endswith(b"\r\n"):
+                print(f"Got fragment: {rx_fragment}")
+                rx_msg += rx_fragment
+                break
+            else:
+                rx_msg += rx_fragment
         self.fl_client.text.clear_rx()
-        return rx_msg
+        # Hack to strip prefix
+        return rx_msg[3:-2]
 
     async def radio_receive_test_task(self):
-        while (True):
+        while True:
             print(await self.radio_receive())
 
     async def radio_receive_task(self, packet_deque: deque):
         print("started radio_receive_task")
         radio_buffer = bytes()
-        while (True):
+        while True:
             radio_buffer = await self.radio_receive()
             print("Received:", radio_buffer)
             packet_deque.append(radio_buffer)
 
     def rig_info(self):
-        print("bandwidth", self.fl_client.rig.bandwidth, "frequency", self.fl_client.rig.frequency,
-            "mode", self.fl_client.rig.mode, "name", self.fl_client.rig.name)
+        print(
+            "bandwidth",
+            self.fl_client.rig.bandwidth,
+            "frequency",
+            self.fl_client.rig.frequency,
+            "mode",
+            self.fl_client.rig.mode,
+            "name",
+            self.fl_client.rig.name,
+        )
 
-    def rig_modify(self, bw='', freq=0.0, mode='', name=''):
-        if (bw != None and bw != ''):
+    def rig_modify(self, bw="", freq=0.0, mode="", name=""):
+        if bw is not None and bw != "":
             self.fl_client.rig.bandwidth = bw
-        if (freq != None and freq != 0.0):
+        if freq is not None and freq != 0.0:
             self.fl_client.rig.frequency = freq
-        if (mode != None and mode != ''):
+        if mode is not None and mode != "":
             self.fl_client.rig.mode = mode
-        if (name != None and name != ''):
-                self.fl_client.rig.name = name
+        if name is not None and name != "":
+            self.fl_client.rig.name = name
 
     def modem_info(self):
-        print("bandwidth", self.fl_client.modem.bandwidth, "carrier", self.fl_client.modem.carrier,
-            "modem", self.fl_client.modem.name)
+        print(
+            "bandwidth",
+            self.fl_client.modem.bandwidth,
+            "carrier",
+            self.fl_client.modem.carrier,
+            "modem",
+            self.fl_client.modem.name,
+        )
 
-    def modem_modify(self, bw=0, carrier=0, modem=''):
-        if (bw != None and bw != 0):
+    def modem_modify(self, bw=0, carrier=0, modem=""):
+        if bw is not None and bw != 0:
             self.fl_client.modem.bandwidth = bw
-        if (carrier != None and carrier != 0):
+        if carrier is not None and carrier != 0:
             self.fl_client.modem.carrier = carrier
             self.fl_client.main.afc = False
-        if (modem != None and modem != '' and self.fl_client.modem.names.count(modem) == 1):
-            if (modem[0:4] == 'BPSK'):
+        if (
+            modem is not None
+            and modem != ""
+            and self.fl_client.modem.names.count(modem) == 1
+        ):
+            if modem[0:4] == "BPSK":
                 self.fl_client.modem.name = modem
 
     def stop(self):
@@ -158,19 +201,22 @@ class fl_instance:
         sleep(self.stop_delay)
         self.fl_app.kill()
 
+
 # Convert raw data in a bytes() object to base64 for radio TX
-def raw_to_base64(raw_bytes, prefix=b'BTC'):
-    base64_buffer = codecs.encode(codecs.decode(raw_bytes.hex(), 'hex'), 'base64')
+def raw_to_base64(raw_bytes, prefix=b"BTC"):
+    base64_buffer = codecs.encode(codecs.decode(raw_bytes.hex(), "hex"), "base64")
     # need to strip the newlines added every 76 bytes; intended for MIME
     # https://docs.python.org/3/library/base64.html#base64.encodebytes
     buffer_mod = len(base64_buffer) // 76
-    stripped_buffer = base64_buffer.replace(b'\n', b'', buffer_mod)
+    stripped_buffer = base64_buffer.replace(b"\n", b"", buffer_mod)
     # add static prefix to assist with accurate decoding
-    return (prefix + stripped_buffer)
+    return prefix + stripped_buffer
 
-# Convert base64-encoded RX radio data to raw bytes() object for port 
+
+# Convert base64-encoded RX radio data to raw bytes() object for port
 def base64_to_raw(base64_bytes):
-    return codecs.decode(base64_bytes, 'base64')
+    return codecs.decode(base64_bytes, "base64")
+
 
 # use a timeout to detect the end of a message from a port
 # increment the timer proportional to data received
@@ -179,21 +225,23 @@ async def port_receive(recv_port: trio.SocketStream, packet_deque: deque):
     print("calling port_receive")
     async for data in recv_port:
         print("port_received", data)
-        if (data != b''):
+        if data != b"":
             packet_deque.append(raw_to_base64(data))
             print("packet queue:", packet_deque)
+
 
 async def port_send(send_port: trio.SocketStream, packet_deque: deque):
     print("calling port_send")
     packet_buffer = bytes()
     poll_delay = 1.0
-    while(True):
-        if (len(packet_deque) > 0):
+    while True:
+        if len(packet_deque) > 0:
             packet_buffer = base64_to_raw(packet_deque.popleft())
             print("port_sending", packet_buffer)
             await send_port.send_all(packet_buffer)
         else:
             await trio.sleep(poll_delay)
+
 
 # Top-level wrappers to handle port->radio and radio->port
 async def port_to_radio(fl_digi: fl_instance, proxy_port: trio.SocketStream):
@@ -204,6 +252,7 @@ async def port_to_radio(fl_digi: fl_instance, proxy_port: trio.SocketStream):
             nursery.start_soon(port_receive, proxy_port, packet_deque)
             nursery.start_soon(fl_digi.radio_send_task, packet_deque)
 
+
 async def radio_to_port(fl_digi: fl_instance, proxy_port: trio.SocketStream):
     print("starting radio_to_port")
     packet_deque = deque()
@@ -212,58 +261,85 @@ async def radio_to_port(fl_digi: fl_instance, proxy_port: trio.SocketStream):
             nursery.start_soon(fl_digi.radio_receive_task, packet_deque)
             nursery.start_soon(port_send, proxy_port, packet_deque)
 
+
 def test_standard():
-    test_strings = ["TEST TEST TEST\n",
+    test_strings = [
+        "TEST TEST TEST\n",
         "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.\n",
-        "The computer can be used as a tool to liberate and protect people, rather than to control them.\n"]
+        "The computer can be used as a tool to liberate and protect people, rather than to control them.\n",
+    ]
     return test_strings
+
 
 def test_raw():
     # Lightning handshake messages captured from lnproxy
-    test_handshake_0 = b'\x00\x03U\xc7\xaa\xa3\x85\xe8%\x95M\x96\xcbQ\x80C\x04\x0f\xf0\x14\xcf\x10\x11{t\x93=\x9d}\xa8a\xf5r\x02w\xca;\x11\xa1T\xaa\x81\xbf\xf2\xcbr\xd5;\xa9\xb2'
-    test_handshake_1 = b'\x00\x02L\xdf\xd9\x81\x98\xcfr\xd8\xa7d\xd2\x167\x98\xff\x9b\t\x16\x1cR\x82^\x96\t8\xfb[\x9fv\x15d\n\xc0\xf7Wi\xf2\x1f\x9f\xd6ht\xba.\xf0>\\\x1c'
-    test_handshake_2 = b'\x00\xae);;\xcd\x02\xea\x12A\xfc@\xb6L\xd6\xd2.\x8by\xfc\xddIR\xd9\x9e\x86\x96j\xbf\x8cA\xec\x8aD\xb0\xf1\xcb\xd6\xedQzq\xc3,\xb3W_\xf25\x0b\x066j\xd7\x06\xd3\xa0\xf0i=\xcd\xd8J\xb0\xffv'
+    test_handshake_0 = b"\x00\x03U\xc7\xaa\xa3\x85\xe8%\x95M\x96\xcbQ\x80C\x04\x0f\xf0\x14\xcf\x10\x11{t\x93=\x9d}\xa8a\xf5r\x02w\xca;\x11\xa1T\xaa\x81\xbf\xf2\xcbr\xd5;\xa9\xb2"
+    test_handshake_1 = b"\x00\x02L\xdf\xd9\x81\x98\xcfr\xd8\xa7d\xd2\x167\x98\xff\x9b\t\x16\x1cR\x82^\x96\t8\xfb[\x9fv\x15d\n\xc0\xf7Wi\xf2\x1f\x9f\xd6ht\xba.\xf0>\\\x1c"
+    test_handshake_2 = b"\x00\xae);;\xcd\x02\xea\x12A\xfc@\xb6L\xd6\xd2.\x8by\xfc\xddIR\xd9\x9e\x86\x96j\xbf\x8cA\xec\x8aD\xb0\xf1\xcb\xd6\xedQzq\xc3,\xb3W_\xf25\x0b\x066j\xd7\x06\xd3\xa0\xf0i=\xcd\xd8J\xb0\xffv"
     test_handshake_3 = b'\x00-\x00\x10\x00\x02"\x00\x00\x03\x02\xaa\xa2\x01 \x06"nF\x11\x1a\x0bY\xca\xaf\x12`C\xeb[\xbf(\xc3O:^3*\x1f\xc7\xb2\xb7<\xf1\x88\x91\x0f'
-    handshakes = [test_handshake_0, test_handshake_1, test_handshake_2, test_handshake_3]
+    handshakes = [
+        test_handshake_0,
+        test_handshake_1,
+        test_handshake_2,
+        test_handshake_3,
+    ]
     handshakes_base64 = []
     hs_test = True
     for hs_message in handshakes:
-        hs_base64 = raw_to_base64(hs_message, prefix=b'')
+        hs_base64 = raw_to_base64(hs_message, prefix=b"")
         print(hs_base64)
         # check for successful newline stripping
-        if (hs_base64.count(b'\n') != 1):
+        if hs_base64.count(b"\n") != 1:
             print("newline stripping failed!")
             hs_test = False
             break
         hs_raw = base64_to_raw(hs_base64)
-        if (hs_raw != hs_message):
+        if hs_raw != hs_message:
             print("encode/decode fail!")
             hs_test = False
             break
         else:
             handshakes_base64.append(hs_base64)
-    if (hs_test == True):
+    if hs_test:
         return handshakes_base64
 
+
 async def main():
-    parser = argparse.ArgumentParser(description='Talk to fldigi.')
-    parser.add_argument('--nodaemon', help="attach to an fldigi process instead of starting one", action="store_true")
-    parser.add_argument('--xml', type=int, help="XML-RPC port")
-    parser.add_argument('--nohead', help='run fldigi without a GUI', action="store_true")
-    parser.add_argument('--noproxy', help="run without TCP proxy functionality", action="store_true")
-    parser.add_argument('--listener', help="run as TCP proxy listener instead of server", action="store_true")
-    parser.add_argument('--proxyport', type=int, help="TCP port for proxy")
-    parser.add_argument('--carrier', type=int, help='set carrier frequency in Hz; disables AFC')
-    parser.add_argument('--modem', type=str, help="select a specific modem")
+    # fmt: off
+    parser = argparse.ArgumentParser(description="Talk to fldigi.")
+    parser.add_argument("--nodaemon", help="attach to an fldigi process instead of starting one", action="store_true",)
+    parser.add_argument("--xml", type=int, help="XML-RPC port")
+    parser.add_argument("--nohead", help="run fldigi without a GUI", action="store_true")
+    parser.add_argument("--noproxy", help="run without TCP proxy functionality", action="store_true")
+    parser.add_argument("--listener", help="run as TCP proxy listener instead of server", action="store_true",)
+    parser.add_argument("--proxyport", type=int, help="TCP port for proxy")
+    parser.add_argument("--carrier", type=int, help="set carrier frequency in Hz; disables AFC")
+    parser.add_argument("--modem", type=str, help="select a specific modem")
+    # fmt: on
     args = parser.parse_args()
-    print("args:", args.nodaemon, args.xml, args.nohead, args.noproxy, args.listener, args.proxyport, args.carrier, args.modem)
+    print(
+        "args:",
+        args.nodaemon,
+        args.xml,
+        args.nohead,
+        args.noproxy,
+        args.listener,
+        args.proxyport,
+        args.carrier,
+        args.modem,
+    )
     # No default port when running as TCP proxy
-    if (args.noproxy == False and args.proxyport == None):
+    if not args.noproxy and args.proxyport is None:
         print("Need a proxy port!")
         return
 
-    fl_main = fl_instance(nodaemon=args.nodaemon, noproxy=args.noproxy, xmlport=args.xml,
-                            proxyport=args.proxyport, headless=args.nohead)
+    fl_main = fl_instance(
+        no_daemon=args.nodaemon,
+        no_proxy=args.noproxy,
+        xml_port=args.xml,
+        proxy_port=args.proxyport,
+        headless=args.nohead,
+    )
     print(fl_main.version())
     fl_main.port_info()
     sleep(fl_main.poll_delay)
@@ -272,33 +348,33 @@ async def main():
     fl_main.modem_info()
     sleep(fl_main.poll_delay)
     fl_main.modem_modify(modem=args.modem, carrier=args.carrier)
-    if (args.modem != None):
+    if args.modem is not None:
         print("modem now", args.modem)
-    if (args.carrier != None):
+    if args.carrier is not None:
         print("carrier frequency now", args.carrier, "Hz, AFC off")
 
     # TCP proxy mode
-    if (args.noproxy == False):
+    if not args.noproxy:
         proxy_stream = await trio.open_tcp_stream("127.0.0.1", args.proxyport)
 
         async with proxy_stream:
             async with trio.open_nursery() as nursery:
-                if (args.listener == True):
+                if args.listener:
                     nursery.start_soon(radio_to_port, fl_main, proxy_stream)
                 else:
                     nursery.start_soon(port_to_radio, fl_main, proxy_stream)
     else:
         # running instance started with custom config
-        if (args.nodaemon):
+        if args.nodaemon:
             print("Attached to fldigi")
-            if (args.noproxy == True):
+            if args.noproxy:
                 fl_main.clear_buffers()
                 async with trio.open_nursery() as nursery:
                     nursery.start_soon(fl_main.radio_receive_test_task)
         # child of this script
         else:
             print("Started fldigi")
-            if (args.noproxy == True):
+            if args.noproxy:
                 test_strings = test_standard()
                 test_base64 = test_raw()
                 async with trio.open_nursery() as nursery:
--- tcp_tester.py
+++ tcp_tester.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3.8
+#!/usr/bin/env python
 
 # Test the fldigi-proxy TCP interface
 
 import argparse
+
 import trio
 
-test_handshake_0 = b'\x00\x03U\xc7\xaa\xa3\x85\xe8%\x95M\x96\xcbQ\x80C\x04\x0f\xf0\x14\xcf\x10\x11{t\x93=\x9d}\xa8a\xf5r\x02w\xca;\x11\xa1T\xaa\x81\xbf\xf2\xcbr\xd5;\xa9\xb2'
-test_handshake_1 = b'\x00\x02L\xdf\xd9\x81\x98\xcfr\xd8\xa7d\xd2\x167\x98\xff\x9b\t\x16\x1cR\x82^\x96\t8\xfb[\x9fv\x15d\n\xc0\xf7Wi\xf2\x1f\x9f\xd6ht\xba.\xf0>\\\x1c'
-test_handshake_2 = b'\x00\xae);;\xcd\x02\xea\x12A\xfc@\xb6L\xd6\xd2.\x8by\xfc\xddIR\xd9\x9e\x86\x96j\xbf\x8cA\xec\x8aD\xb0\xf1\xcb\xd6\xedQzq\xc3,\xb3W_\xf25\x0b\x066j\xd7\x06\xd3\xa0\xf0i=\xcd\xd8J\xb0\xffv'
+test_handshake_0 = b"\x00\x03U\xc7\xaa\xa3\x85\xe8%\x95M\x96\xcbQ\x80C\x04\x0f\xf0\x14\xcf\x10\x11{t\x93=\x9d}\xa8a\xf5r\x02w\xca;\x11\xa1T\xaa\x81\xbf\xf2\xcbr\xd5;\xa9\xb2"
+test_handshake_1 = b"\x00\x02L\xdf\xd9\x81\x98\xcfr\xd8\xa7d\xd2\x167\x98\xff\x9b\t\x16\x1cR\x82^\x96\t8\xfb[\x9fv\x15d\n\xc0\xf7Wi\xf2\x1f\x9f\xd6ht\xba.\xf0>\\\x1c"
+test_handshake_2 = b"\x00\xae);;\xcd\x02\xea\x12A\xfc@\xb6L\xd6\xd2.\x8by\xfc\xddIR\xd9\x9e\x86\x96j\xbf\x8cA\xec\x8aD\xb0\xf1\xcb\xd6\xedQzq\xc3,\xb3W_\xf25\x0b\x066j\xd7\x06\xd3\xa0\xf0i=\xcd\xd8J\xb0\xffv"
 test_handshake_3 = b'\x00-\x00\x10\x00\x02"\x00\x00\x03\x02\xaa\xa2\x01 \x06"nF\x11\x1a\x0bY\xca\xaf\x12`C\xeb[\xbf(\xc3O:^3*\x1f\xc7\xb2\xb7<\xf1\x88\x91\x0f'
-handshakes = [test_handshake_0, test_handshake_1, test_handshake_2, test_handshake_3]
+handshakes = [test_handshake_0] #, test_handshake_1, test_handshake_2, test_handshake_3]
+
 
 async def send_raw(proxy_port, packets):
     print("Beginning to serve raw packets")
@@ -19,6 +21,7 @@ async def send_raw(proxy_port, packets):
         # space out packets
         await trio.sleep(0.1)
 
+
 async def tester_client(output_stream):
     print("fldigi output client started")
     received_data = []
@@ -26,11 +29,12 @@ async def tester_client(output_stream):
         async for data in output_stream:
             print("receiving data:", data)
             received_data.append(data)
-            if (len(received_data) == len(handshakes)):
-                if (received_data == handshakes):
+            if len(received_data) == len(handshakes):
+                if received_data == handshakes:
                     print("Successful proxying!")
     except Exception as exc:
         print("tester client crashed: {!r}".format(exc))
+
 
 async def tester_server(input_stream):
     print("fldigi input server started")
@@ -43,13 +47,16 @@ async def tester_server(input_stream):
     except Exception as exc:
         print("tester server crashed: {!r}".format(exc))
 
+
 async def server_wrapper(inport):
     print("wrapping server")
-    await trio.serve_tcp(tester_server, inport) 
+    await trio.serve_tcp(tester_server, inport)
+
 
 async def client_wrapper(outport):
     print("wrapping client")
-    await trio.serve_tcp(tester_client, outport) 
+    await trio.serve_tcp(tester_client, outport)
+
 
 async def main():
     parser = argparse.ArgumentParser(description="test fldigi-proxy")
@@ -61,6 +68,7 @@ async def main():
     async with trio.open_nursery() as nursery:
         nursery.start_soon(server_wrapper, args.inport)
         nursery.start_soon(client_wrapper, args.outport)
+
 
 if __name__ == "__main__":
     trio.run(main)
